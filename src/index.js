307ac41f9bf7b28da40afe250bd20c6f13edc3b7
misko_16 <119707421+misko16@users.noreply.github.com>
2023-10-16 13:54:33 -0700

    hope

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import App from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
 import  store  from 'redux/store';
+import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
- 
+
+  <BrowserRouter basename='/goit-react-hw-06-phonebook'>
   <Provider store={store}> 
     <App />
-  </Provider> 
+  </Provider>
+  </BrowserRouter>
  );
