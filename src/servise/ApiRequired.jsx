58e9e78fdf6ccebc0d84b50e4ffe4b21e4fc7175
misko_16 <119707421+misko16@users.noreply.github.com>
2023-10-26 14:36:35 -0700

    hapynes

--- src/servise/ApiRequired.jsx
+++ src/servise/ApiRequired.jsx
@@ -5,7 +5,7 @@ const ApiUrl = `https://6539a46fe3b530c8d9e88f85.mockapi.io/contacts/contacts`;
 
 axios.defaults.baseURL(ApiUrl);
 
-export const fetchRequest = createAsyncThunk('contacts/fetchAll', async(state, action) => {
+export const fetchRequest = createAsyncThunk('contacts/fetchAll', async() => {
     const response = await axios.get('/contacts');
     return response.data;
 });
